277d2069c7ba9e2889f5fe32b56c5785e84c276b
Kumar, Roshan <rkunm@allstate.com>
2020-10-19 23:34:26 +0530

    NoTicket: update dependency

--- Package.swift
+++ Package.swift
@@ -5,13 +5,12 @@ import PackageDescription
 let package = Package(
     name: "sampleproject",
     products: [
-        .library(name: "sampleproject", targets: ["sampleproject"]),
+        .library(name: "sampleproject", type: .dynamic, targets: ["sampleproject"]),
     ],
     dependencies: [
         .package(url: "https://github.com/danger/swift.git", from: "3.0.0"),
     ],
     targets: [
-        .target(name: "sampleproject", dependencies: [], path: "sampleproject"),
-        .testTarget(name: "sampleprojectTests", dependencies: ["sampleproject"], path: "sampleprojectTests"),
+        .target(name: "sampleproject", dependencies: ["Danger"], path: "sampleproject")
     ]
 )
